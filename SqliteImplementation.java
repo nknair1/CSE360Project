d01446317dd54adfe69eb81d8e6d30d05edd7496
josemadge09 <147361766+josemadge09@users.noreply.github.com>
2024-11-23 05:09:41 -0700

    SqliteImplementation.java

--- SqliteImplementation.java
+++ SqliteImplementation.java
@@ -4,21 +4,20 @@ import java.sql.*;
 
 public class SqliteImplementation {
 
-	public static Connection connect() {
+    public static Connection connect() {
         Connection conn = null; 
         try {
             Class.forName("org.sqlite.JDBC");
             conn = DriverManager.getConnection("jdbc:sqlite:Database36.sqlite");
             System.out.println("Connection to SQLite database has been established.");
         } catch (SQLException e) {
-            System.out.println("Connection to SQLite failed.");// + e.getMessage());
+            System.out.println("Connection to SQLite failed.");
         } catch (ClassNotFoundException e) {
-            System.out.println("JDBC driver not found.");// + e.getMessage());
+            System.out.println("JDBC driver not found.");
         }
         return conn; 
     }
 
- 
     public static boolean insertUser(String firstName, String lastName, String email, String confirmEmail, String password, String confirmPassword, String userType) {
         String sql = "INSERT INTO users (name, last_name, email, confirm_email, password, confirm_password, user_type) VALUES (?, ?, ?, ?, ?, ?, ?)";
         try (Connection conn = connect(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
@@ -32,29 +31,27 @@ public class SqliteImplementation {
             pstmt.executeUpdate();
             return true;
         } catch (SQLException e) {
-            System.out.println("Insert user failed.");// + e.getMessage());
+            System.out.println("Insert user failed.");
             return false;
         }
     }
 
- 
-    public static String confirmLogin(String email, String password) {
-        String sql = "SELECT user_type FROM users WHERE email = ? AND password = ?";
+    public static User confirmLogin(String email, String password) {
+        String sql = "SELECT name, last_name, user_type FROM users WHERE email = ? AND password = ?";
         try (Connection conn = connect(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
             pstmt.setString(1, email);
             pstmt.setString(2, password);
             ResultSet rs = pstmt.executeQuery();
             if (rs.next()) {
-                return rs.getString("user_type");
+                String firstName = rs.getString("name");
+                String lastName = rs.getString("last_name");
+                String userType = rs.getString("user_type");
+                String fullName = firstName + " " + lastName;
+                return new User(fullName, email, userType);
             }
         } catch (SQLException e) {
-            System.out.println("Login validation failed.");// + e.getMessage());
+            System.out.println("Login validation failed.");
         }
         return null;
     }
-    
-  
-    
 }
-
-
